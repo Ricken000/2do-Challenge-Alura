3352c1e0e031fcd41eb16bcf71a86fa8244d2a8e
Ricardo Caviedes Febre <ric.caviedes.feb@gmail.com>
2022-05-14 17:07:38 -0400

    se añaden las clases letter y hidden a la palabra jugada

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,16 @@
+const words = ['auto']
+const wordContainer = document.getElementById('wordContainer');
+
+let word = words[0]
+
+for (let index = 0; index < word.length; index++) {
+    let selectedword = document.createElement('span');
+    selectedword.textContent = word[index];
+    selectedword.classList.add('letter');
+    selectedword.classList.add('hidden');
+    wordContainer.appendChild(selectedword);
+}
 
 document.addEventListener('keydown', (event) =>{
     const keyname = event.key;
 });
-
