5f4363088cdd2611a2357fe2d960744460d4789b
Ricardo Caviedes Febre <ric.caviedes.feb@gmail.com>
2022-05-15 12:39:12 -0400

    detecta tecla presionada, compara y borra

--- Script/script.js
+++ Script/script.js
@@ -1,16 +1,33 @@
 const words = ['auto']
 const wordContainer = document.getElementById('wordContainer');
+const buttonNewGame = document.getElementById('newGame');
+let letter;
 
 let word = words[0]
 
-for (let index = 0; index < word.length; index++) {
-    let selectedword = document.createElement('span');
-    selectedword.textContent = word[index];
-    selectedword.classList.add('letter');
-    selectedword.classList.add('hidden');
-    wordContainer.appendChild(selectedword);
+function showLetter (){
+    for (let index = 0; index < word.length; index++) {
+        let selectedword = document.createElement('span');
+        selectedword.textContent = word[index];
+        selectedword.classList.add('letter');
+        selectedword.classList.add('hidden');
+        wordContainer.appendChild(selectedword);
+    }
 }
 
-document.addEventListener('keydown', (event) =>{
-    const keyname = event.key;
-});
+
+
+function newGame (){
+    buttonNewGame.style.display = 'none';
+    showLetter();
+    document.addEventListener('keydown',(Event) =>{
+        for (let index = 0; index < wordContainer.childElementCount; index++) {
+            letter = wordContainer.children[index]
+            if (letter.textContent == Event.key) {
+                letter.classList.remove('hidden');
+            }
+            console.log(wordContainer.children[index].textContent);
+        }
+    });
+}
+
